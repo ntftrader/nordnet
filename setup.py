ae319484f291308e148ca0b3bbbff4a111a6aa09
ntftrader <ntftrader@gmail.com>
2020-01-19 19:53:07 +0100

    Setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nordnet",
+    version="0.1.0",
+    author="NTF Trader",
+    author_email="ntftrader@gmail.com",
+    description="A wrapper for nordnet.no public apis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ntftrader/nordnet",
+    packages=setuptools.find_packages(),
+    install_requires=['requests', 'simplejson', 'pandas'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
